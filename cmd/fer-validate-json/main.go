73a7753cebff5d22b1b9c5760c08817a9dd7f309
Sebastien Binet <binet@cern.ch>
2016-11-09 18:10:20 +0100

    cmd/fer-validate-json: detect multiple 'channel|device|socket' keys

--- cmd/fer-validate-json/main.go
+++ cmd/fer-validate-json/main.go
@@ -6,10 +6,14 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"flag"
+	"fmt"
+	"io"
 	"log"
 	"os"
+	"reflect"
 )
 
 type Config struct {
@@ -17,19 +21,36 @@ type Config struct {
 }
 
 type Options struct {
-	Devices []Device `json:"devices"`
+	Devices []Device `json:"devices,omitempty"`
 	Device  Device   `json:"device"`
 }
 
+func (opts Options) MarshalJSON() ([]byte, error) {
+	if opts.Device.isZero() {
+		return json.Marshal(struct {
+			Devices []Device `json:"devices"`
+		}{
+			Devices: opts.Devices,
+		})
+	}
+	return json.Marshal(struct {
+		Devices []Device `json:"devices,omitempty"`
+		Device  Device   `json:"device"`
+	}{
+		Devices: opts.Devices,
+		Device:  opts.Device,
+	})
+}
+
 type Device struct {
-	ID       string    `json:"id"`
-	Key      string    `json:"key"`
-	Channels []Channel `json:"channels"`
+	ID       string    `json:"id,omitempty"`
+	Key      string    `json:"key,omitempty"`
+	Channels []Channel `json:"channels,omitempty"`
 	Channel  Channel   `json:"channel"`
 }
 
 func (dev *Device) isZero() bool {
-	return dev.ID == "" && dev.Key == ""
+	return dev.ID == "" && dev.Key == "" && len(dev.Channels) == 0
 }
 
 func (dev *Device) name() string {
@@ -39,10 +60,57 @@ func (dev *Device) name() string {
 	return dev.Key
 }
 
+func (dev Device) MarshalJSON() ([]byte, error) {
+	if dev.Channel.isZero() {
+		return json.Marshal(struct {
+			ID       string    `json:"id,omitempty"`
+			Key      string    `json:"key,omitempty"`
+			Channels []Channel `json:"channels,omitempty"`
+		}{
+			ID:       dev.ID,
+			Key:      dev.Key,
+			Channels: dev.Channels,
+		})
+	}
+	return json.Marshal(struct {
+		ID       string    `json:"id,omitempty"`
+		Key      string    `json:"key,omitempty"`
+		Channels []Channel `json:"channels,omitempty"`
+		Channel  Channel   `json:"channel"`
+	}{
+		ID:       dev.ID,
+		Key:      dev.Key,
+		Channels: dev.Channels,
+		Channel:  dev.Channel,
+	})
+}
+
 type Channel struct {
 	Name    string   `json:"name"`
-	Sockets []Socket `json:"sockets"`
+	Sockets []Socket `json:"sockets,omitempty"`
 	Socket  Socket   `json:"socket"`
+}
+
+func (ch *Channel) MarshalJSON() ([]byte, error) {
+	if ch.Socket.isZero() {
+		return json.Marshal(struct {
+			Name    string   `json:"name"`
+			Sockets []Socket `json:"sockets,omitempty"`
+		}{
+			Name:    ch.Name,
+			Sockets: ch.Sockets,
+		})
+	}
+
+	return json.Marshal(struct {
+		Name    string   `json:"name"`
+		Sockets []Socket `json:"sockets,omitempty"`
+		Socket  Socket   `json:"socket"`
+	}{
+		Name:    ch.Name,
+		Sockets: ch.Sockets,
+		Socket:  ch.Socket,
+	})
 }
 
 func (ch *Channel) isZero() bool {
@@ -50,12 +118,12 @@ func (ch *Channel) isZero() bool {
 }
 
 type Socket struct {
-	Type        string `json:"type"`
-	Method      string `json:"method"`
-	Address     string `json:"address"`
-	SendBufSize string `json:"sndBufSize"`
-	RecvBufSize string `json:"rcvBufSize"`
-	RateLogging string `json:"rateLogging"`
+	Type        string `json:"type,omitempty"`
+	Method      string `json:"method,omitempty"`
+	Address     string `json:"address,omitempty"`
+	SendBufSize string `json:"sndBufSize,omitempty"`
+	RecvBufSize string `json:"rcvBufSize,omitempty"`
+	RateLogging string `json:"rateLogging,omitempty"`
 }
 
 func (sck *Socket) isZero() bool {
@@ -72,7 +140,8 @@ func main() {
 		os.Exit(1)
 	}
 
-	run(*verbose)
+	validate(*verbose)
+	// run(*verbose)
 }
 
 func run(verbose bool) {
@@ -124,4 +193,61 @@ func run(verbose bool) {
 	if !allgood {
 		log.Fatalf("[%s] validation FAILED\n", fname)
 	}
+}
+
+func validate(verbose bool) {
+	fname := flag.Arg(0)
+	f, err := os.Open(fname)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	w := new(bytes.Buffer)
+	var cfg Config
+	err = json.NewDecoder(f).Decode(&cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = json.NewEncoder(w).Encode(cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	chk := linearize(w)
+
+	_, err = f.Seek(0, 0)
+	if err != nil {
+		log.Fatal(err)
+	}
+	ref := linearize(f)
+
+	if !reflect.DeepEqual(ref, chk) {
+		log.Fatalf("[%s] validation FAILED:\nref=%v\nchk=%v\n", fname, string(ref), string(chk))
+	}
+}
+
+func linearize(r io.Reader) []byte {
+	str := new(bytes.Buffer)
+	dec := json.NewDecoder(r)
+	for {
+		t, err := dec.Token()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		switch t := t.(type) {
+		case json.Delim:
+			fmt.Fprintf(str, "%v", t)
+		default:
+			s := ""
+			if dec.More() {
+				s = " "
+			}
+			fmt.Fprintf(str, "%v%s", t, s)
+		}
+	}
+	return str.Bytes()
 }

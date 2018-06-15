8564a99aaa5f7c1b34561c46ff7cd24294a4a560
Sebastien Binet <binet@cern.ch>
2018-06-15 15:24:47 +0200

    fer,mq/czmq: add CGo-based czmq transport plugin
    This CL adds back the CGo-based ZeroMQ transport (using pebbe/zmq4.)
    This transport known as "czmq" to the fer plugin, is guarded behind the
    "czmq" build tag (so fer is pure-Go by default.)

---
+++ czmq_plugin.go
@@ -0,0 +1,11 @@
+// Copyright 2018 The fer Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+//+build czmq
+
+package fer
+
+import (
+	_ "github.com/sbinet-alice/fer/mq/czmq" // load C-bindings zeromq plugin
+)
